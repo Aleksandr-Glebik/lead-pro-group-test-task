55d951e3b59639bb3ecbdd52856281766ad17f78
Aleksandr-Glebik <glebikaa@mail.ru>
2023-09-22 20:48:58 +0300

    init: start project

---
+++ README.md
@@ -0,0 +1,1 @@
+#### LeadProGroup test task
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      init project
+    </div>
+  )
+}
+
+export default App
