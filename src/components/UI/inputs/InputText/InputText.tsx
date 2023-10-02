7a7ba53886fb48f38c1447824fb93197d541f893
Aleksandr-Glebik <glebikaa@mail.ru>
2023-10-02 21:46:52 +0300

    fix: change style for invalid status, change validation for InputText

--- src/components/UI/inputs/InputText/InputText.tsx
+++ src/components/UI/inputs/InputText/InputText.tsx
@@ -4,22 +4,23 @@ import clsx from 'clsx';
 import { ActionType } from '../../../../reducer';
 import { useStateContext } from '../../../../rootState';
 import styles from './InputText.module.css'
-import { lengthValidator } from '../../../../utils/lengthValidator';
 
-let initialMessage = 'Поле не должно быть пустым'
+let invalidMessage = 'Введите имя'
+const regex = new RegExp(`^[a-zA-Z][a-zA-Z0-9-_\\.]{3,70}$`)
 
 const InputText = () => {
   const { state, dispatch } = useStateContext()
   const { name } = state;
 
   const [valid, setValid] = useState(true)
-  const [invalidMessage, setInvalidMessage] = useState(initialMessage)
 
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value) {
-      const { message, isValid } = lengthValidator(event.target.value, 3, 100)
-      setInvalidMessage(message)
-      setValid(isValid)
+    if (regex.test(event.target.value)) {
+      setValid(true)
+    }
+
+    if (!regex.test(event.target.value)) {
+      setValid(false)
     }
 
     if (dispatch) {
@@ -28,7 +29,6 @@ const InputText = () => {
   }
 
   return (
-
     <div className={styles.container}>
       <input
         type='text'
