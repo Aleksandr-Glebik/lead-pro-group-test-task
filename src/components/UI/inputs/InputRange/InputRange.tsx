2f86283355e524fc5a6a3ace5822a80dc10d43f9
Aleksandr-Glebik <glebikaa@mail.ru>
2023-10-05 20:50:37 +0300

    feat: add style for InputRang: output, thumb, add adaptive

--- src/components/UI/inputs/InputRange/InputRange.tsx
+++ src/components/UI/inputs/InputRange/InputRange.tsx
@@ -1,10 +1,14 @@
+import { useEffect, useRef } from 'react'
 import styles from './InputRange.module.css';
 import { useStateContext } from '../../../../rootState';
 import { ActionType } from '../../../../reducer';
+import { stepCount } from '../../../../utils/stepCount';
 
 const InputRange = () => {
   const { state, dispatch } = useStateContext()
   const { age } = state;
+  const outputRef = useRef<HTMLOutputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (dispatch) {
@@ -12,9 +16,30 @@ const InputRange = () => {
     }
   };
 
+  const changeOutputPosition = (age: number) => {
+    if (outputRef.current && inputRef.current) {
+      if (age) {
+        let inputWidth = inputRef.current.clientWidth
+        let widthStep = Math.round(inputWidth / (35 - 17))
+        let leftPositionOutput = stepCount(+age) * widthStep + 'px'
+        outputRef.current.style.left = leftPositionOutput
+      }
+    }
+  }
+
+  useEffect(() => {
+    changeOutputPosition(+age)
+  }, [age])
+
   return (
     <div className={styles.container}>
-      <div className={styles.container__age}>{age}</div>
+      <output
+        htmlFor='age'
+        className={styles.output}
+        ref={outputRef}
+      >
+        {age}
+      </output>
       <input
         type="range"
         id="age"
@@ -24,7 +49,8 @@ const InputRange = () => {
         step="1"
         className={styles.container__input}
         onChange={inputHandler}
-        defaultValue={age}
+        defaultValue={age === '' ? age : 26}
+        ref={inputRef}
       />
       <label className={styles.container__label}>
         <span className={styles.container__label_text}>18 лет</span>
---
+++ src/utils/stepCount.ts
@@ -0,0 +1,42 @@
+export const stepCount = (num: number): number => {
+    switch (num) {
+        case 18:
+            return 0
+        case 19:
+            return 1
+        case 20:
+            return 2
+        case 21:
+            return 3
+        case 22:
+            return 4
+        case 23:
+            return 5
+        case 24:
+            return 6
+        case 25:
+            return 7
+        case 26:
+            return 8
+        case 27:
+            return 9
+        case 28:
+            return 10
+        case 29:
+            return 11
+        case 30:
+            return 12
+        case 31:
+            return 13
+        case 32:
+            return 14
+        case 33:
+            return 15
+        case 34:
+            return 16
+        case 35:
+            return 17
+        default:
+            return 27
+    }
+}
